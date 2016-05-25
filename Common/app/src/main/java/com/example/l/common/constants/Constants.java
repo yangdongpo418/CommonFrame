f6d928b1b726cf6f2579722eef08701098d9f2ce
dongpo <ydpyycyr@163.com>
2016-05-26 00:30:07 +0800

    add loading state page

--- Common/app/src/main/java/com/example/l/common/constants/Constants.java
+++ Common/app/src/main/java/com/example/l/common/constants/Constants.java
@@ -15,4 +15,10 @@ public class Constants {
     //这个广播是注销登陆会发送的
     public static final String INTENT_ACTION_LOGOUT = "Action.LOGOUT";
     public static final String INTENT_ACTION_LOGIN = "Action.LOGIN";
+
+    /**------------网络请求的状态码，用来更新界面----------------*/
+    public static final int STATE_SUCCESS = 0;
+    public static final int STATE_ERROR = 1;
+    public static final int STATE_LOADING = 2;
+    public static final int STATE_EMPTY = 3;
 }
